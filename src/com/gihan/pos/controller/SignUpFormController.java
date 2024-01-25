4f12f02ceccd2929af707d290f5de6a808ce4049
Your Name <darkcreationsl98@gmail.com>
2024-01-25 23:59:04 +0530

    created sign up without encryption

--- src/com/gihan/pos/controller/SignUpFormController.java
+++ src/com/gihan/pos/controller/SignUpFormController.java
@@ -5,10 +5,15 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SignUpFormController {
 
@@ -17,6 +22,24 @@ public class SignUpFormController {
     public JFXPasswordField txtPassWord;
 
     public void btnRegisterNowOnAction(ActionEvent actionEvent) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+          Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/robotikka2",
+                    "root","1234");
+          String sql="INSERT INTO user VALUES (?,?)";
+          PreparedStatement preparedStatement=connection.prepareStatement(sql);
+          preparedStatement.setString(1,txtEmail.getText());
+          preparedStatement.setString(2,txtPassWord.getText());
+         if ( preparedStatement.executeUpdate()>0){
+             new Alert(Alert.AlertType.CONFIRMATION,"Registration Success").show();
+         }else {
+             new Alert(Alert.AlertType.WARNING,"Try Again").show();
+         }
+        } catch (ClassNotFoundException | SQLException e) {
+         new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+        }
+
+
     }
 
     public void btnAlreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
