ea3ad397f65e46224b8099c2829f020ce95ac1f9
Your Name <darkcreationsl98@gmail.com>
2024-01-25 23:24:26 +0530

    finalize login and signup ui

--- src/com/gihan/pos/controller/LoginFormController.java
+++ src/com/gihan/pos/controller/LoginFormController.java
@@ -1,4 +1,33 @@
 package com.gihan.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txt_password;
+
+    public void btnSigninOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignUpForm");
+    }
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml")))
+        );
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
